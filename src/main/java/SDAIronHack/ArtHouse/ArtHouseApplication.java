74a23a9e762a0ee8b9bb3b8b94c940847ea37d79
YumnaAlansari <yumnaalansariid@gmail.com>
2023-10-19 14:42:01 +0300

    Initial commit

---
+++ src/main/java/SDAIronHack/ArtHouse/ArtHouseApplication.java
@@ -0,0 +1,13 @@
+package SDAIronHack.ArtHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SDAIronHack/ArtHouse/ArtHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package SDAIronHack.ArtHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
